6421f7b86358f096db82c62dcc41e3e5d2aa2b86
Thomas Cooper <tcooper@twitter.com>
2018-05-03 16:48:34 -0700

    Moved all quick test help scripts and other files with internal urls to untacked folders

--- tests/metrics/heron/cuckoo/test_client.py
+++
@@ -1,7 +0,0 @@
-from caladrius.metrics.heron.cuckoo.client import HeronCuckooClient
-
-config = {"cuckoo.database.url": 'https://cuckoo-prod-smf1.twitter.biz'}
-
-cuckoo = HeronCuckooClient(config, "Infra-Caladrius")
-
-print("Cookoo metrics client available as: cuckoo")
