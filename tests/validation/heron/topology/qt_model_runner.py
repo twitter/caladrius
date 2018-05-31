ac4a67b9d14186c310b1298e7b3cdee69fe7e888
Thomas Cooper <tcooper@twitter.com>
2018-05-30 17:45:33 -0700

    Added csv save to validation

--- tests/validation/heron/topology/qt_model_runner.py
+++ tests/validation/heron/topology/qt_model_runner.py
@@ -23,28 +23,17 @@ from caladrius.metrics.heron.client import HeronMetricsClient
 from caladrius.model.topology.heron.queueing_theory import QTTopologyModel
 from caladrius.tests.validation.heron import helpers as heron_helper
 from caladrius.tests.validation import helpers as validation_helper
+from caladrius.common.heron import zookeeper
 
 LOG: logging.Logger = logging.getLogger(__name__)
 
 
 def compare(metrics_client: HeronMetricsClient,
             spout_state: Dict[int, Dict[str, float]],
+            actual_instance_arrs: pd.DataFrame,
             topology_model: QTTopologyModel,
             topology_id: str, cluster: str, environ: str, start: dt.datetime,
             end: dt.datetime, metric_bucket_length: int, **kwargs):
-
-    # ## ARRIVAL RATES ##
-
-    # Get the actual arrival rates at all instances
-    actual_arrs: pd.DataFrame = \
-        metrics_client.get_arrival_rates(topology_id, cluster, environ, start,
-                                         end, **kwargs)
-
-    actual_instance_arrs: pd.DataFrame = \
-        (actual_arrs.groupby(["component", "task"])["arrival_rate_tps"]
-         .mean().reset_index()
-         .rename(index=str, columns={"arrival_rate_tps":
-                                     "actual_arrival_rates_tps"}))
 
     # Predict the arrival rates from the spout state during this period
     predicted_instance_arrs, stmgr_arrs = \
@@ -71,42 +60,93 @@ def run(config: Dict[str, Any], metrics_client: HeronMetricsClient,
         topology_model: QTTopologyModel, topology_id: str, cluster: str,
         environ: str, metric_bucket_length: int, **kwargs: Any):
 
+    start: dt.datetime = (dt.datetime.utcnow().replace(tzinfo=dt.timezone.utc)
+                          - dt.timedelta(hours=total_hours))
+
+    zk_config = config["heron.topology.models.config"]
+    last_updated: dt.datetime = zookeeper.last_topo_update_ts(
+        zk_config["heron.statemgr.connection.string"],
+        zk_config["heron.statemgr.root.path"], topology_id,
+        zk_config["zk.time.offset"])
+
+    if start < last_updated:
+        update_err: str = (f"The provided total hours ({total_hours}) will "
+                           f"result in a start time ({start.isoformat()}) "
+                           f"which is before the last update to "
+                           f"{topology_id}'s physical plan "
+                           f"({last_updated.isoformat()})")
+        LOG.error(update_err)
+        raise RuntimeError(update_err)
+
     periods: List[Tuple[dt.datetime, dt.datetime]] = \
         validation_helper.create_start_end_list(total_hours,
                                                 period_length_secs)
 
     output: pd.DataFrame = None
 
-    for i, (start, end) in enumerate(periods):
+    for i, (source_start, source_end) in enumerate(periods):
+        LOG.info("\n\n\nComparing period %d of %d\n\n", i, len(periods))
+        LOG.info("Using metrics sourced from %s to %s",
+                 source_start.isoformat(), source_end.isoformat())
 
-        try:
-            LOG.info("Comparing period %d of %d from %s to %s", i,
-                     len(periods), start.isoformat(), end.isoformat())
+        for j, (traffic_start, traffic_end) in enumerate(periods):
 
-            spout_state = heron_helper.get_spout_state(
-                metrics_client, topology_id, cluster, environ,
-                config["heron.tracker.url"], start, end, 60, "mean")
-
-            results: pd.DataFrame = compare(
-                metrics_client, spout_state, topology_model, topology_id,
-                cluster, environ, start, end, metric_bucket_length, **kwargs)
-
-        except ConnectionRefusedError as cr_err:
-            LOG.error("Connection was refused with message: %s", str(cr_err))
-        except ConnectionResetError as cre_err:
-            LOG.error("Connection was reset with message: %s", str(cre_err))
-        except requests.exceptions.ConnectionError as req_err:
-            LOG.error("Connection error with message: %s", str(req_err))
-        except Exception as err:
-            LOG.error("Error (%s) with message: %s", str(type(err)), str(err))
-        else:
-            results["period_start"] = start
-            results["period_end"] = end
-
-            if output is not None:
-                output = output.append(results, ignore_index=True)
+            if (traffic_start, traffic_end) == (source_start, source_end):
+                continue
             else:
-                output = results
+                LOG.info("\n\nComparing prediction, using metrics from period "
+                         "%d and traffic from period %d, to actual performance"
+                         " during period %d\n", i, j, j)
+
+                LOG.info("Using traffic data from period between %s and %s",
+                         traffic_start, traffic_end)
+
+            try:
+
+                spout_state = heron_helper.get_spout_state(
+                    metrics_client, topology_id, cluster, environ,
+                    config["heron.tracker.url"], traffic_start, traffic_end,
+                    60, "mean")
+
+                # Get the actual arrival rates at all instances
+                actual_arrs: pd.DataFrame = \
+                    metrics_client.get_arrival_rates(topology_id, cluster,
+                                                     environ, traffic_start,
+                                                     traffic_end, **kwargs)
+
+                actual_instance_arrs: pd.DataFrame = \
+                    (actual_arrs.groupby(["component", "task"])
+                     ["arrival_rate_tps"].mean().reset_index()
+                     .rename(index=str, columns={"arrival_rate_tps":
+                                                 "actual_arrival_rates_tps"}))
+
+                results: pd.DataFrame = compare(
+                    metrics_client, spout_state, actual_instance_arrs,
+                    topology_model, topology_id,
+                    cluster, environ, source_start, source_end,
+                    metric_bucket_length, **kwargs)
+
+            except ConnectionRefusedError as cr_err:
+                LOG.error("Connection was refused with message: %s",
+                          str(cr_err))
+            except ConnectionResetError as cre_err:
+                LOG.error("Connection was reset with message: %s",
+                          str(cre_err))
+            except requests.exceptions.ConnectionError as req_err:
+                LOG.error("Connection error with message: %s", str(req_err))
+            except Exception as err:
+                LOG.error("Error (%s) with message: %s", str(type(err)),
+                          str(err))
+            else:
+                results["source_start"] = source_start
+                results["source_end"] = source_end
+                results["traffic_start"] = traffic_start
+                results["traffic_end"] = traffic_end
+
+                if output is not None:
+                    output = output.append(results, ignore_index=True)
+                else:
+                    output = results
 
     return output
 
@@ -127,6 +167,9 @@ def _create_parser() -> argparse.ArgumentParser:
     parser.add_argument("--debug", required=False, action="store_true",
                         help=("Optional flag indicating if debug level "
                               "information should be displayed"))
+    parser.add_argument("-od", "--output_dir", required=False,
+                        help=("Optional output directory to save results "
+                              "DataFrames too."))
 
     parser.add_argument("-t", "--topology", required=True)
     parser.add_argument("-c", "--cluster", required=True)
@@ -177,7 +220,10 @@ if __name__ == "__main__":
         ARGS.topology, ARGS.cluster, ARGS.environ,
         int(CONFIG["heron.topology.models.config"]["metric.bucket.length"]))
 
-    print(results.to_string())
+    if ARGS.output_dir:
+        results.to_csv(os.path.join(ARGS.output_dir, "arrival_rates.csv"))
+
+    print("\n#### ARRIVAL RATE VALIDATION ####\n")
 
     print("\nError overall:\n")
     print(results.error.describe().to_string())
